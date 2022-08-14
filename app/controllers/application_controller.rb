0338aa9dd3c3bdd2a46c333344c9f825474592cb
Danny Rodriguez <danny007111@hotmail.com>
2022-08-14 15:37:44 -0500

    Updated server, created sinatra CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,29 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Add `/reviews` to directory!" }.to_json
+  end
+
+  get "/reviews" do
+    reviews = Review.all.order(:created_at)
+    reviews.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(review: params[:review], dish_id: dish.id, user_id: self)
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    reviewed = Review.find(params[:id])
+    reviewed.update(review: params[:review])
+    reviewed.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 
 end
